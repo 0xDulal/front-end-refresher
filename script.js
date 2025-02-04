4fb8d1a9585d572320b2e09fea0e6445fe5676a2
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-02-04 14:01:32 +0600

    main

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("name").innerHTML = "<br>JavaScript!";
+console.warn("This is a warning!");
+console.error("This is an error!");
+var a, b;
+a= 10;
+b= 20;
+console.log(a+b);
+
+var name = prompt("Enter your name: ");
+console.log(name);
