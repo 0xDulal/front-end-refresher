3dd6361890604e4494dc27fd63bd50426d1e3f1f
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-02-12 13:28:07 +0600

    done

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById("name").innerHTML = "<br>JavaScript!";
 console.warn("This is a warning!");
 console.error("This is an error!");
 var a, b;
@@ -6,5 +5,9 @@ a= 10;
 b= 20;
 console.log(a+b);
 
-var name = prompt("Enter your name: ");
-console.log(name);
+// var name_ok = prompt("Enter your name: ");
+// document.getElementById("name").innerHTML = name_ok;
+// console.log(name_ok);
+document.getElementById("btn-pop").onclick = function() {
+    alert("Hello, World!");
+}
