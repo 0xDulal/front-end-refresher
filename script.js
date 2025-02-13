d10c1810d32265139e56df8c479260c9210dfda2
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-02-13 18:05:05 +0600

    Update script

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 console.warn("This is a warning!");
 console.error("This is an error!");
+
 var a, b;
-a= 10;
+a= 150;
 b= 20;
 console.log(a+b);
 
 // var name_ok = prompt("Enter your name: ");
 // document.getElementById("name").innerHTML = name_ok;
 // console.log(name_ok);
-document.getElementById("btn-pop").onclick = function() {
-    alert("Hello, World!");
-}
+if(a>b) {console.log("Yes");} else {
+        console.log("No");
+    }
