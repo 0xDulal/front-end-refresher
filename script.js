9b78d3bf3b1915cc60aa28be7b76fc08457111da
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-02-18 16:32:00 +0600

    ss

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ console.error("This is an error!");
 var a, b;
 a= 150;
 b= 20;
-console.log(a+b);
+console.log(+b);a
 
-// var name_ok = prompt("Enter your name: ");
-// document.getElementById("name").innerHTML = name_ok;
-// console.log(name_ok);
-if(a>b) {console.log("Yes");} else {
-        console.log("No");
-    }
+var name_ok = prompt("Enter your name: ");
+document.getElementById("name").innerHTML = name_ok;
+console.log(name_ok);
+// if(a>b) {console.log("Yes");} else {
+//         console.log("No");
+//     }
